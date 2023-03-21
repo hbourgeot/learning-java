5206b69999c0c8b9c9fc5f0c3ac14aa33104b34b
Estudiandev <henrry.bourgeot@linktic.com>
2023-03-20 22:40:31 -0400

    first commit

---
+++ src/main/java/logic/Logic.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package logic;
+
+import gui.Screen;
+
+/**
+ *
+ * @author Admin
+ */
+public class Logic {
+
+    public static void main(String[] args) {
+        Screen screen = new Screen();
+        screen.setVisible(true);
+        screen.setLocationRelativeTo(null);
+        
+    }
+}
